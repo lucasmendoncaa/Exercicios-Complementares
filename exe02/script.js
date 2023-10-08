a22d7b625541e140c59b2b10ab82bd495e075191
lucasmendoncaa <lucasdecarmo@gmail.com>
2023-10-07 23:48:06 -0300

    delet parseFloat e atribuindo variavel correta

--- exe02/script.js
+++ exe02/script.js
@@ -5,8 +5,8 @@ function calcularDesconto() {
     var captarQuantidade = parseFloat(document.getElementById('quantidadeproduto').value);
     var captarDesconto = parseFloat(document.getElementById('descontoproduto').value);
 
-    var calcularResultado = parseFloat(document.getElementById('resultado'));
+    var calcularResultado = document.getElementById('resultado');
 
     var calcularValorEQuantidade = (captarNome * captarQuantidade);
-    resultado.innerText = "O valor do produto fica em: " + (calcularValorEQuantidade - [calcularValorEQuantidade * (captarDesconto / 100)]).toFixed(2);
+    calcularResultado.innerText = "O valor do produto fica em: " + (calcularValorEQuantidade - [calcularValorEQuantidade * (captarDesconto / 100)]).toFixed(2);
 }
