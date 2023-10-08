60a65a8e79eb445e9c9c4fea09092baa50171ca9
lucasmendoncaa <lucasdecarmo@gmail.com>
2023-10-08 00:27:13 -0300

    inital commit

---
+++ exe03/README.md
@@ -0,0 +1,3 @@
+# Linguagem e Padrões Web
+# descrição:
+Considere que a aprovação de um aluno em determinada disciplina requer uma média final maior ou igual a 6,0. Elabore uma página web que receba duas notas, realize o cálculo da média, exiba o valor calculado e uma imagem indicando se o aluno está aprovado ou reprovado.
---
+++ exe03/script.js
@@ -0,0 +1,22 @@
+
+
+function carregarImagem() {
+    var imagem = document.getElementById('imagem');
+    var frase = document.getElementById('resultado');
+    var n1 = parseFloat(document.getElementById('nota1').value);
+    var n2 = parseFloat(document.getElementById('nota2').value);
+
+    var media = (n1 + n2) / 2;
+
+
+    if (media >= 6) {
+        imagem.src = "/exe03/imagens/aprovado.png";
+        frase.innerText = "Aprovado";
+        frase.innerText = "Sua média é " + media + ", você foi Aprovado!";
+        return true;
+    }else {
+        imagem.src = "/exe03/imagens/reprovado.jfif";
+        frase.innerText = "Sua média é " + media + ", você foi Reprovado!";
+        return false;
+    }
+}
